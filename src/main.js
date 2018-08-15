87a6d632f14cbdfd4d292e69d7ec9dc90ade3108
zybeargithub <1106408264qq.com>
2018-08-15 16:02:14 +0800

    开始阅读源码

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
-import fastclick from 'fastclick'
-import VueLazyload from 'vue-lazyload'
+import fastclick from 'fastclick' // fastclick：处理移动端click事件300毫秒延迟
+import VueLazyload from 'vue-lazyload' // 资源懒加载，移步github
 
+// 关闭生产模式下的提示
 Vue.config.productionTip = false
 
+// 懒加载 default.png 图片
 Vue.use(VueLazyload, {
   loading: require('common/image/default.png')
 })
